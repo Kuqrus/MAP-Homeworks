4b6c565234305b1bf533444b1db239e0bba08d3f
Kuqrus <kuqrus@gmail.com>
2023-08-01 23:47:11 +0500

    Add task 3

---
+++ Task-3/ParallelForEach.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include <iterator>
+#include <thread>
+
+#include "Timer.h"
+
+void add(int& x);
+
+template <typename Iterator, typename Function>
+void parallel_for_each(Iterator first, Iterator last, Function func);
+
+void InitForeach();
